3d0d45d291654b7510d65b063d7cda38a5091f21
rigels777 <35729605+rigels777@users.noreply.github.com>
2018-01-23 12:59:48 +0100

    Add files via upload

---
+++ header.php
@@ -0,0 +1,36 @@
+<html>
+<head>
+
+
+    <title> </title>
+    <link rel="stylesheet" type="text/css" href="style.css"
+
+
+</head>
+<body>
+
+
+<header>
+
+    <nav>
+        <div class="main-wrapper">
+            <ul>
+                <li> <a href="index.php"> Home </a> </li>
+            </ul>
+            <div class="nav-login">
+                <form>
+
+                    <input type="text" name="username" placeholder="Username/E-mail">
+                    <input type="password" name="password" placeholder="password">
+                    <button type="submit" name="submit"> Login </button>
+                </form>
+
+                <a href="signup.php"> Sign Up </a>
+
+            </div>
+
+        </div>
+
+    </nav>
+
+</header>
---
+++ signup.php
@@ -0,0 +1,31 @@
+<?php
+
+include_once 'header.php';
+?>
+<section class="main-container">
+    <div class="main-wrapper">
+
+        <h2> Sign Up </h2>
+
+        <form class="signup-form">
+            <center>
+            <input type="text" name="first" placeholder="Firstname">
+            <input type="text" name="last" placeholder="Last Name">
+            <input type="text" name="email" placeholder="E-mail">
+            <input type="text" name="username" placeholder="Username">
+            <input type="password" name="password" placeholder="Password">
+            <button type="submit" name="submit"> Sign Up  </button>
+
+
+</center>
+        </form>
+
+    </div>
+
+</section>
+
+
+<?php
+
+include_once 'footer.php';
+?>
